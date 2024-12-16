45fc2c5fff049ffdae2c642a9b6e143cd185c9d8
tigerfats <burrclaw@gmail.com>
2024-12-15 20:29:42 -0600

    Update sizes.js

--- javascripts/sizes.js
+++ javascripts/sizes.js
@@ -84,7 +84,7 @@ var newDragon = function(event){
 	}
 	
 	if(d_breed !="everluxf"){
-		wings.style.left = -d_wingspan/4 + d_length/11;
+		wings.style.left = -d_wingspan/4 + d_length/2;
 	}
 	
 	if(d_breed !="everluxm"){
